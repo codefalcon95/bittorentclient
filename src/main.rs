1bf973147bb3933c2c2d7de2f21a471c6213ddaf
Sanni Moshood Abiola <sanniabiolamoshood95@gmail.com>
2023-11-17 00:09:16 +0100

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,17 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    // we want to confirm if the first character of a string is number
+    let is_first_char_digit = match encoded_value.chars().next() {
+        Some(c) => c.is_digit(10),
+        None => false
+    };
+    if !is_first_char_digit {
+        return serde_json::Value::String(encoded_value.to_string());
     }
+
+    let splitted_encoded_string: Vec<&str> = encoded_value.split(':').collect();
+    return serde_json::Value::String(splitted_encoded_string[1].to_string());
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
@@ -26,13 +27,50 @@ fn main() {
 
     if command == "decode" {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", serde_json::to_string(&decoded_value).unwrap());
     } else {
         println!("unknown command: {}", args[1])
+    }
+}
+
+#[allow(dead_code)]
+fn get_length_from_bencoded_string(encoded_string: &str) -> Option<usize> {
+    let encoded_string_splitted: Vec<&str> = encoded_string.split(':').collect();
+    if encoded_string_splitted.len() != 2 {
+        return Option::None;
+    }
+
+    let length = match encoded_string_splitted[0].parse::<usize>() {
+        Ok(length) => Some(length),
+        Err(_) => return None,
+    };
+    return length;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_length_from_bencoded_string() {
+        let encoded_string = "4:spam";
+        let length = get_length_from_bencoded_string(encoded_string);
+        assert_eq!(Some(4), length);
+    }
+
+    #[test]
+    fn test_no_length_from_bencoded_string() {
+        let encoded_string = "spam";
+        let length = get_length_from_bencoded_string(encoded_string);
+        assert_eq!(None, length);
+    }
+
+    #[test]
+    fn test_decode_bencoded_value() {
+        let encoded_string = "4:spam";
+        let decoded_value = decode_bencoded_value(encoded_string);
+        assert_eq!(serde_json::Value::String("spam".to_string()), decoded_value);
     }
 }
